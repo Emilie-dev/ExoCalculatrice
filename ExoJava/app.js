85c6e08fc8cc90d31c2bd3adef24cf0f0b3e1226
Chauvin <chauvin-e@orange.fr>
2017-03-09 14:15:49 +0100

    Modification final d'ExoJava

--- ExoJava/app.js
+++ ExoJava/app.js
@@ -1,5 +1,5 @@
 function addition (a, b) {
-return a + b;
+	return a + b;
 }
 console.log(addition(2, 4));
 
@@ -13,3 +13,17 @@ function multiplication (a, b) {
 	return a * b;
 }
 console.log(multiplication(5, 3));
+
+//Si b > 0 console.log affichera le résultat de la division.
+//Si b =< 0 console.log affichera "Can't divide by 0".
+
+function division(a, b) {
+	if (b > 0) {
+		return a / b;
+	}else{
+		console.log(division("Can't divide by 0"))
+	}
+}
+console.log(division(4, 2));
+
+
